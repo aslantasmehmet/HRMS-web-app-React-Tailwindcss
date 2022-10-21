ae67758287b21c31bf6ec0d643ba90e58a306ff9
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-10-21 14:44:25 +0300

    With the React router, the login and registration buttons in the Header section were directed to the address.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,7 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <div className="bg-white ">
@@ -25,9 +29,16 @@ export default function Header() {
             <button className="mr-4 h-10 w-32 border-2 border-cyan-200 font-semibold  bg-neutral-50 rounded text-cyan-300 hover:bg-cyan-300 hover:text-white">
               İlan Paketi Al
             </button>{" "}
-            <button className="bg-purple-700 hover:bg-purple-800 border rounded-lg h-10 w-48 text-white font-semibold">
-              Giriş Yap/Üye Ol
-            </button>
+            <NavLink to={"/aday/giris"}>
+              <button className=" bg-purple-700 hover:bg-purple-800 border rounded-lg h-10 w-32 text-white font-semibold">
+                Giriş Yap
+              </button>
+            </NavLink>
+            <NavLink to={"/aday/uyeol"}>
+              <button className=" bg-purple-700 hover:bg-purple-800 border rounded-lg h-10 w-32 text-white font-semibold">
+                Üye Ol
+              </button>
+            </NavLink>
           </div>
         </div>
       </div>
