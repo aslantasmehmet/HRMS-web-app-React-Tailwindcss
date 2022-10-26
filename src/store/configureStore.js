448589545b8bd9d69d579bf8819494615916e0fe
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-10-26 20:00:15 +0300

    Login is now working on localStorage and keep process.

---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from "redux";
+import { persistStore } from "redux-persist";
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from "./rootReducers";
+import { loadState, saveState } from "../localStorage"
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+
+export function configureStore() {
+    const persistedState = loadState()
+    
+    return createStore(rootReducer, persistedState, composedEnhancer)
+}
