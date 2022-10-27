5c6e06f0ba4ee289f3583bd9b46ec52a072e8b2d
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-10-28 00:11:23 +0300

    Edited

--- src/layout/Dashboard.js
+++ src/layout/Dashboard.js
@@ -3,6 +3,7 @@ import HomePage from "./HomePage";
 import LoginPage from "./LoginPage"
 import İndex from "../pages/Signin/İndex"
 import { Route, Routes } from "react-router-dom";
+import CandidateProfile from "../pages/Profile/CandidateProfile"
 
 export default function Dashboard() {
   return (
@@ -12,6 +13,7 @@ export default function Dashboard() {
         <Route exact path="/anasayfa" element={<HomePage />} />
         <Route exact path="/aday/giris" element={<LoginPage />} />
         <Route exact path="/aday/uyeol" element={<İndex />} />
+        <Route exact path="/aday/profile" element={<CandidateProfile />} />
       </Routes>
     </div>
   );
