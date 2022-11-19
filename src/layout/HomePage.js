7133adb46c634f432cef819af5f3e2684d358b4f
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-11-20 01:26:32 +0300

    react slick is included in the system.

--- src/layout/HomePage.js
+++ src/layout/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header"
 import HeroSection from "../components/HeroSection"
+import Highlights from "../components/Highlights";
 
 export default function HomePage() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <Highlights/>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-color": "#4c3398",
+        "primary-brand-color": "#5d3ebc",
+        "secondary-brand-color": "#7849f7",
+        "brand-yellow": "#ffd300",
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
